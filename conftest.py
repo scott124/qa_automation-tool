7c4b482290f49afbbc210b7756a2e60badb5881e
SHIRO\user <scottskying@yahoo.com.tw>
2025-06-28 15:21:03 +0800

    文字&test_case更新_06281520

--- conftest.py
+++ conftest.py
@@ -14,10 +14,8 @@ def env(request):
 # 再生成allure報告前，先刪除舊的json報告
 @pytest.hookimpl(tryfirst=True)
 def pytest_sessionstart(session):
-    """在 pytest 執行前，自動清空 allure 報告資料夾內容"""
     report_dir = "allure-results"
     if os.path.exists(report_dir):
-        # 只清空內容
         for f in os.listdir(report_dir):
             fpath = os.path.join(report_dir, f)
             if os.path.isfile(fpath):
